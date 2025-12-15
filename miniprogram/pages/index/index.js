2f9620d73cd9b7513dc673efb99728c62a2e163f
tcyeee <tcyeee@outlook.com>
2025-12-15 18:48:54 +0800

    UI init

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,13 +1,36 @@
 Page({
   data: {
     avatarUrl: "",
-    defaultAvatar: "https://dummyimage.com/200x200/ededed/999999&text=Avatar"
+    defaultAvatar: "https://dummyimage.com/200x200/ededed/999999&text=Avatar",
+    hasAvatar: false,
+    hatAdded: false,
   },
   
   onChooseAvatar(e) {
     const { avatarUrl } = e.detail || {};
     if (avatarUrl) {
-      this.setData({ avatarUrl });
+      this.setData({
+        avatarUrl,
+        hasAvatar: true,
+        hatAdded: false,
+      });
     }
+  },
+
+  onAddHat() {
+    // TODO: 在此处添加圣诞帽生成逻辑
+    this.setData({ hatAdded: true });
+  },
+
+  onDownload() {
+    // TODO: 替换为实际下载逻辑
+    wx.showToast({ title: "下载中...", icon: "none" });
+  },
+
+  onShareAppMessage() {
+    return {
+      title: "画个圣诞帽",
+      path: "/pages/index/index",
+    };
   },
 });
