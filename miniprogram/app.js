8a6f0c5dfcdba89729d27d9f0ec015a44ac0720d
tcyeee <tcyeee@outlook.com>
2025-12-18 14:51:11 +0800

    add application version check.

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,5 +3,36 @@ import { initCloud } from "./apis/http";
 App({
   onLaunch: function () {
     initCloud();
+    this.checkForUpdate();
+  },
+
+  checkForUpdate() {
+    if (!wx.canIUse || !wx.canIUse("getUpdateManager"))       return;
+
+    const updateManager = wx.getUpdateManager();
+
+    updateManager.onCheckForUpdate(() => {
+      // 仅在有新版本时处理提示与更新
+    });
+
+    updateManager.onUpdateReady(() => {
+      wx.showModal({
+        title: "更新提示",
+        content: "检测到新版本，是否重启并应用更新？",
+        success: (res) => {
+          if (res.confirm) {
+            updateManager.applyUpdate();
+          }
+        },
+      });
+    });
+
+    updateManager.onUpdateFailed(() => {
+      wx.showModal({
+        title: "更新失败",
+        content: "新版本下载失败，请稍后重试",
+        showCancel: false,
+      });
+    });
   },
 });
